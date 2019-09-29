f0e0066e72d87f1faa67a3533e684ce8d6d824cc
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-29 22:09:27 +0600

    class component testing done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './myComponents/Hello'
+import Welcome from './myComponents/Welcome'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Hello></Hello>
+        <Welcome></Welcome>
       </header>
     </div>
   );
