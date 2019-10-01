381e07ce155712a1cbcc71a6b35aff392ba71d24
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-01 18:34:03 +0600

    setState use to change state value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Arraw from './myComponents/Arraw'
+// import Arraw from './myComponents/Arraw'
 import States from './myComponents/States'
+import Hello from './myComponents/Hello';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <States></States>
-        <Arraw></Arraw>
+        {/* <Arraw></Arraw> */}
+        <Hello></Hello>
       </header>
     </div>
   );
--- src/myComponents/Hello.js
+++ src/myComponents/Hello.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-function Hello(){
+class Hello extends Component{
 
-    // function DoThis(){
-    //     alert()
-    // }
+   constructor(){
+      super()
 
-    return(
-        <div>
-           <button className="btn btn-info">Test Bootstrap</button>
-        </div>
-   );
+      this.state={
+         name: "Shahajahan"
+      }
+   }
+
+   changeName(a){
+
+      this.setState({name:a})
+
+   }
+
+
+   render(){
+      return <div className="text-success">
+          <h1>{this.state.name}</h1>
+          <button onClick={this.changeName.bind(this,"Rain")} className="btn btn-info">change name</button>
+      </div>
+  }
+  
 }
+
+// function Hello(){
+
+//     // function DoThis(){
+//     //     alert()
+//     // }
+
+//     return(
+//         <div>
+//            <button className="btn btn-info">Test Bootstrap</button>
+//         </div>
+//    );
+// }
 
 export default Hello;
