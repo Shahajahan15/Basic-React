f1147dba6135d9cb0a5c70223d7ca86fa8658964
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-01 17:15:56 +0600

    Nested State And Array Property

--- src/myComponents/States.js
+++ src/myComponents/States.js
@@ -8,18 +8,24 @@ class States extends Component {
 
         this.state={
             name:"Shahajahan",
-            age: 26,
+            age: [26,45,64,32,43],   //arrary declaration
             height: "5 feet 3 inch",
-            weight: "52 KG"
+            weight: {
+                class1: 12,
+                class2: 1,
+                class3: [5,6,9,10,3],
+                class4: 7
+            }
         }
+
     }
 
     render(){
         return <div className="text-success">
             <h1>{this.state.name}</h1>
-            <h1>{this.state.age}</h1>
+            <h1>{this.state.age[3]}</h1>
             <h1>{this.state.height}</h1>
-            <h1>{this.state.weight}</h1>
+            <h1>{this.state.weight.class3[2]}</h1>
         </div>
     }
 }
