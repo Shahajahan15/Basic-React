dad2a9248c5d47595805a4072a0297ab14adc29a
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-02 16:53:39 +0600

    event handler in the onChange

--- src/myComponents/Forms.js
+++ src/myComponents/Forms.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Forms extends Component {
+
+    constructor (){
+        super()
+        this.state={
+            Name: "  "
+        }
+    }
+
+    onChangeHan=(event)=>{
+        var newName = event.target.value;
+        this.setState({name:newName})
+    }
+
     render() {
         return (
             <div>
                 <form>
                     <p>My first form</p>
-                <input type="text" name="name" placeholder="Enter Name"></input>
-                <input type="submit" name="submit" value="submit"></input>
+                    <p>{this.state.name}</p>
+                <input onChange={this.onChangeHan} type="text" placeholder="Enter Name"></input>
+                <input type="submit" value="submit"></input>
                 </form>
             </div>
         );
