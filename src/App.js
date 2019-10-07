3be021be3273f5d89de3f1c92b3300914403467d
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-07 22:48:16 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import HookDemo from './hooks/HookDemo';
+// import HookDemo from './hooks/HookDemo';
+import Son from './ComponentTree/Son';
 
 
 
 function App() {
   return (
     <div className="App">
-      <HookDemo></HookDemo>
+      <Son></Son>
     </div>
   );
 }
