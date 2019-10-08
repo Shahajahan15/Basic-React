60dc60111d406747bdcdf5b3ae2e15c64b7d8ef0
Shahajahan15 <mdrezaali1@gmail.com>
2019-10-08 13:19:58 +0600

    use Fragment alternative to div tag

--- src/ComponentTree/Puti.js
+++ src/ComponentTree/Puti.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 import './../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { PutiConsumer } from './myContext';
 
 class Puti extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <h1 className="text-success">{this.props.name}</h1>
                 <h1 className="text-info">
                     <PutiConsumer>
@@ -16,7 +16,7 @@ class Puti extends Component {
                         }
                     </PutiConsumer>
                 </h1>
-            </div>
+            </Fragment>
         );
     }
 }
